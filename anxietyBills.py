a1adbc03551552be4f8e56e497c6ded234fed410
IonianIronist <36776432+IonianIronist@users.noreply.github.com>
2020-08-08 16:30:57 +0300

    adding stuff
    added command line arguments, balance, delete and file insertion options

--- anxietyBills.py
+++ anxietyBills.py
@@ -1,66 +1,78 @@
-import re
-import sqlite3
-from sqlite3 import Error
-
 import fn
 from create_tables import create_connection
+import argparse
+import sys
 
 
-def main(conn):
-    executor = conn.cursor()
+def main(connection, executor):
     while True:
         print("1) Insert a new transaction")
         print("2) View transactions")
+        print("3) View balance")
+        print("4) Delete transactions")
+        print("5) Insert transactions from csv")
         print("q) Exit")
         rep = input("-->")
-        if rep is "1":
-            while True:
-                print("Input date yyyy-mm-dd")
-                rep = input("-->")
-                rexp = re.compile(r"[0-9]{4}(-[0-9]{2}){2}")
-                if rexp.match(rep) is not None:
-                    date = rep
-                    break
-                else:
-                    print("Wrong type input")
-
-            print("Input amount spent/recived\nfor info input '-h'")
-            rep = input("-->")
-            while True:
-                if rep == r"-h":
-                    print(
-                        """If you want to declare a negative transaction the
-                        input should be
-                        -(amount)
-                        a positive transaction should be just (amount)
-                        The amount should always be followed by two float digits
-                        i.e. 1234.56 """)
-                    rep = input("-->")
-                rexp = re.compile(r"-?[0-9]+\.[0-9]{2}")
-                if rexp.match(rep) is not None:
-                    amount = rep
-                    break
-                else:
-                    print("Wrong type input")
-
-            print("Input a description of the transaction")
-            description = input("-->")
-            fn.insert_into_transactions(executor, date, amount, description)
-            conn.commit()
-        elif rep is "2":
-            tupples = fn.select_all(executor)
-            rows = tupples.fetchall()
+        if rep == "1":
+            fn.insert_transaction(executor, connection)
+        elif rep == "2":
+            print("(ID, DATE, AMOUNT, DESCRIPTION)")
+            rows = fn.select_all(executor)
             for row in rows:
                 print(row)
-        elif rep is "q":
-            print("DONT PANIC")
+        elif rep == "3":
+            print(fn.balance(executor))
+        elif rep == "4":
+            print("Witch transactions to delete ?\n eg. ---> 21,2,3,8")
+            rows = fn.select_all(executor)
+            for row in rows:
+                print(row)
+            rows = [int(x) for x in input().split(',')]
+            fn.delete_transaction(rows, executor)
+        elif rep == "5":
+            file = input("Input file name\n--->")
+            fn.file_insertion(executor, file)
+        elif rep == "q":
+            print("DON'T PANIC")
             break
         else:
             print("Wrong input")
-            conn.close()
+            print("DON'T PANIC")
+            connection.close()
             break
 
 
 if __name__ == '__main__':
     conn = create_connection(r"C:\sqlite\db\anxiety_db.db")
-    main(conn)
+    exe = conn.cursor()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--insert", help="Insert a transaction\nanxietyBills -i yyyy-mm-dd,amount,description",
+                        required=False)
+    parser.add_argument("-fi", "--fileinsertion", help="Insert transactions from a file\nanxietyBills -fi path",
+                        required=False)
+    parser.add_argument("-b", "--balance", help="See the money balance\nanxietyBills -b", required=False,
+                        action='store_true')
+    parser.add_argument("-v", "--view", help="View all transactions", required=False, action='store_true')
+    args = parser.parse_args()
+    if args.insert:
+        transaction = args.insert.split(',')
+        if fn.date_check(transaction[0]) and fn.amount_check(transaction[1]) and transaction.len() == 3:
+            fn.insert_into_transactions(exe, transaction[0], float(transaction[1]), transaction[2])
+        else:
+            print("Wrong transaction input")
+            sys.exit()
+    elif args.fileinsertion:
+        fn.file_insertion(exe, args.fileinsertion)
+        sys.exit()
+    elif args.view:
+        print("(ID, DATE, AMOUNT, DESCRIPTION)")
+        rs = fn.select_all(exe)
+        for r in rs:
+            print(r)
+        sys.exit()
+    elif args.balance:
+        print("The Balance:")
+        print(fn.balance(exe))
+        sys.exit()
+    print(args)
+    main(conn, exe)
